0f8640fbc6e9e1de80fb9f04d5ddf6cf5bcf3908
MuhammadAbdullah76 <jamalabdullah1229@gmail.com>
2024-01-31 13:02:12 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Crud-App
+ A Crud app using php for practice and upgrade my skills 
---
+++ updatedata.php
@@ -0,0 +1,18 @@
+<?php
+
+$stu_id = $_POST['sid'];
+$stu_name = $_POST['sname'];
+$stu_add  = $_POST['saddress'];
+$stu_class = $_POST['sclass'];
+$stu_phone = $_POST['sphone'];
+
+include 'config.php';
+
+$sql = "UPDATE student SET sname = '{$stu_name}', saddress = '{$stu_add}', sclass = '{$stu_class}',sphone='{$stu_phone}' WHERE sid = {$stu_id} ";
+$result = mysqli_query($conn, $sql) or die ("Query Unseccessful.");
+
+header("Location: http://localhost/crud/index.php"); 
+
+mysqli_close($conn);
+
+?>
